a3be530497aed326727008f11423bb37858a509a
Kris <kristina_lacrima@mail.ru>
2023-12-03 20:22:35 +0300

    initiall

---
+++ src/test/java/PostmanEchoTest.java
@@ -0,0 +1,37 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+class PostmanEchoTest {
+
+    @Test
+    public void ReturnSendData() {
+
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("Hello") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo( "Hello"));
+    }
+
+    @Test
+    public void NotReturnSendData() {
+
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("Hello") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo( "Bye"));
+    }
+}
