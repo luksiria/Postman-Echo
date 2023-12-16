6b1a172e919dd6941d0bd962aa3cb7c94f552fd1
Kris <kristina_lacrima@mail.ru>
2023-12-16 13:27:58 +0300

    update test

--- src/test/java/PostmanEchoTest.java
+++ src/test/java/PostmanEchoTest.java
@@ -20,33 +20,4 @@ class PostmanEchoTest {
                 .body("data", equalTo( "Hello"));
     }
 
-    @Test
-    public void NotReturnSendData() {
-
-        given()
-                .baseUri("https://postman-echo.com")
-                .body("Hello") // отправляемые данные (заголовки и query можно выставлять аналогично)
-// Выполняемые действия
-                .when()
-                .post("/post")
-// Проверки
-                .then()
-                .statusCode(200)
-                .body("data", equalTo( "Bye"));
-    }
-
-    @Test
-    public void ReturnSendNull() {
-
-        given()
-                .baseUri("https://postman-echo.com")
-                .body("null") // отправляемые данные (заголовки и query можно выставлять аналогично)
-// Выполняемые действия
-                .when()
-                .post("/post")
-// Проверки
-                .then()
-                .statusCode(200)
-                .body("data", equalTo( "null"));
-    }
 }
